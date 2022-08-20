a868f4c5fc513d5beba82228bd29ae1c982aeaf9
kenluu3 <ken.luuu@hotmail.com>
2022-08-20 11:53:33 -0400

    adding cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 import express from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { dataSource } from './config';
 import { accountRouter, videoRouter, favoriteRouter } from './routes';
 
+
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
