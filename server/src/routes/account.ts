c3f3561d4be68e9492949fb6cd63e0b82021121b
kenluu3 <ken.luuu@hotmail.com>
2022-08-25 18:57:38 -0400

    update method

--- server/src/routes/account.ts
+++ server/src/routes/account.ts
@@ -6,7 +6,7 @@ import { accountValidator } from '../validators';
 const router = express.Router();
 router.get('/:username', accountValidator.getValidation, accountController.getAccount);
 router.post('/register', accountValidator.registerValidation, accountController.registerAccount);
-router.post('/update', accountValidator.updateValidation, passport.authenticate('jwt', { session: false}), accountController.updateAccount);
+router.patch('/update', accountValidator.updateValidation, passport.authenticate('jwt', { session: false}), accountController.updateAccount);
 router.post('/login', accountValidator.loginValidation, accountController.loginAccount);
 
 export { router as accountRouter };
