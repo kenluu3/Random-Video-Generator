f5ca7af67103d9e5776fa1981432110e3f980820
kenluu3 <ken.luuu@hotmail.com>
2022-08-31 17:21:26 -0400

    updated error page

--- client/src/pages/Error/Error.tsx
+++ client/src/pages/Error/Error.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { Text } from '@mantine/core';
-import { Navigation } from '../../components';
+import { useNavigate } from 'react-router-dom'; 
+import { Button, Stack, Text, Title } from '@mantine/core';
+import { appRoutes } from '../../app';
+
+import '../../styles/base-page.scss';
 
 const Error = () => {
+  const navigate = useNavigate();
+  const handleReturn = () => navigate(appRoutes.home);
+
   return (
-    <Navigation>
-      <Text>
-        404 Error Page Not Found
-      </Text>
-    </Navigation>
+    <div className='page-container'>
+      <Stack justify='space-between' align='center'>
+        <Title order={1} align='center'>
+          404 - PAGE NOT FOUND
+        </Title>
+        <Text>
+          The page you are looking for may have been removed or is temporarily unavailable.
+        </Text>
+        <Button radius='xl' onClick={handleReturn} >
+          Return to Homepage
+        </Button>
+      </Stack>
+    </div>
   )
 }
 
