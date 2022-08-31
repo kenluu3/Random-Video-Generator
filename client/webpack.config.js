4d1c332788b46837a7a4d901abf692f556d1c3c6
kenluu3 <ken.luuu@hotmail.com>
2022-08-31 17:19:11 -0400

    sass deps

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,8 +19,8 @@ module.exports = {
         type: 'asset/resource',
       },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ]
   },
